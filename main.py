87265388c934a8188fcb4e7980cbca35c1ea7e4d
Marcin Brzezinski <xbrzezinx@gmail.com>
2025-04-29 12:13:56 +0200

    main.py push

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
+
 conn = sqlite3.connect("X:\\Hashsets\\RDS_2025.03.1_ios_minimal\\RDS_2025.03.1_ios_minimal.db")
 cursor = conn.cursor()
+
 with open('X:\\Hashsets\\NIST_ios_HashSet.txt', 'w', encoding='utf-8') as f:
     for row in cursor.execute('SELECT md5 FROM FILE'):
         f.write(''.join(map(str, row)) + '\n')
