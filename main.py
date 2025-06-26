d3eaf8b9a9ff3c81eeae995d5a91fbe91ff9ad36
Marcin Brzezinski <xbrzezinx@gmail.com>
2025-06-26 13:43:40 +0200

    Change SELECT to SELECT DISTINCT and sort by md5

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ conn = sqlite3.connect("X:\\Hashsets\\RDS_2025.03.1_ios_minimal\\RDS_2025.03.1_i
 cursor = conn.cursor()
 
 with open('X:\\Hashsets\\NIST_ios_HashSet.txt', 'w', encoding='utf-8') as f:
-    for row in cursor.execute('SELECT md5 FROM FILE'):
+    for row in cursor.execute('SELECT DISTINCT md5 FROM FILE ORDER BY md5'):
         f.write(''.join(map(str, row)) + '\n')
