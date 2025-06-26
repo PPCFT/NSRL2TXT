c0ea5bde44ed21bc4e3c033f8d8bbb6d8ea03651
Marcin Brzezinski <xbrzezinx@gmail.com>
2025-06-26 13:55:58 +0200

    Add header 'MD5' to txt file

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ conn = sqlite3.connect("X:\\Hashsets\\RDS_2025.03.1_ios_minimal\\RDS_2025.03.1_i
 cursor = conn.cursor()
 
 with open('X:\\Hashsets\\NIST_ios_HashSet.txt', 'w', encoding='utf-8') as f:
+    f.write("MD5\n")  # Add header for X-Ways
     for row in cursor.execute('SELECT DISTINCT md5 FROM FILE ORDER BY md5'):
         f.write(''.join(map(str, row)) + '\n')
